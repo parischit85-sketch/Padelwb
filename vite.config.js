623cecc81513d4adb798c8507249c9975a999598
Paris <parischit85@sketch.com>
2025-09-05 23:15:35 +0200

    Fix: Add conditional base path for Netlify and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Padelwb/',
+  base: process.env.GITHUB_ACTIONS ? '/Padelwb/' : process.env.NETLIFY ? '/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
